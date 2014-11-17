a2050255f83e01cec2faa270816bad811c1828de
nileshrajpurohit <nilesh.rajpurohit@resagit.in>
2014-11-17 11:43:39 +0530

    Check In by nIlesh

--- GITSample/src/com/git/testing/GITTest.java
+++ GITSample/src/com/git/testing/GITTest.java
@@ -5,7 +5,7 @@ public class GITTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("first Commit");
-			System.out.println("Initial Check in of GIT");
+			System.out.println("Initial Check in Nilesh on Git");
 	}
 
 }
